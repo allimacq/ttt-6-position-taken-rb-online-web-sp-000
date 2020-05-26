ca598977c7ff14bf2de2d829f437e871f35fba81
Allison MacQueen <allimacq@gmail.com>
2020-05-26 19:40:21 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,8 +2,8 @@
 def position_taken?(board, index)
   if board[index] == " " || ""
     p false
-    elsif board[index] == "X" || "O"
-      p true
+    elsif board[index] != "X" || "O"
+      p false
     elsif board[index] = "nil"
       p nil
   end
