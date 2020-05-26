df90523f70726ed836fe8e90777c6ebd32b7f689
Allison MacQueen <allimacq@gmail.com>
2020-05-26 19:42:41 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,10 +1,10 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if board[index] == " " || ""
+  if board[index] == " " || board[index] == ""
     p "false"
     elsif board[index] == "nil"
       p "nil"
-      elsif board[index] == "X" || board[index] == "O"
-        p "true"
+    elsif board[index] == "X" || board[index] == "O"
+      p "true"
   end
 end
